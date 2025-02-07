7bb68cddd4706f9f054e0c9f46f1142afd62cfa1
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-02-07 17:46:40 +0200

    fix bags

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ import { getAccessTokenAPI } from './api_access';
 import { getAlbum, getNewReleases } from './api_album';
 import { createPlayList } from './api_playlist';
 import { fetchProfile } from './api_users';
+import "./header.js";
 
 const params = new URLSearchParams(window.location.search);
 let code = params.get("code");
