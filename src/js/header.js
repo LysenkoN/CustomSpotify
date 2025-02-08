9b83f6ba84419413a6248c840e470fb44b93e0e4
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-02-08 11:04:25 +0200

    update help

--- src/js/header.js
+++ src/js/header.js
@@ -4,21 +4,30 @@ const profileAvatar = document.querySelector(".header-profile-avatar");
 const homeBtn = document.querySelector(".header-search-btn-home");
 const itemHelpHome = document.querySelector(".help-home");
 const itemHelpProfile = document.querySelector(".help-profile");
+const logo = document.querySelector(".header-logo-img");
+const itemHelpLogo = document.querySelector(".help-header-logo-img");
+const search = document.querySelector(".svg-search");
+const itemHelpSearch = document.querySelector(".help-svg-search");
+
 let id
-profileAvatar.addEventListener("mouseover",()=>{
-    id = setTimeout(()=>{itemHelpProfile.style.display = "flex";},700);
-});
 
-profileAvatar.addEventListener("mouseout",()=>{
-    itemHelpProfile.style.display = "none";
+function visible(value){
+    id = setTimeout(()=>{value.style.display = "flex";},700);
+}
+
+function unvisible(value){
+    value.style.display = "none";
     clearTimeout(id);
-});
+}
+profileAvatar.addEventListener("mouseover",()=>{visible(itemHelpProfile)});
+profileAvatar.addEventListener("mouseout",()=>{unvisible(itemHelpProfile)});
 
- homeBtn.addEventListener("mouseover",()=>{
-     id = setTimeout(()=>{itemHelpHome.style.display = "flex";},700);
- });
 
- homeBtn.addEventListener("mouseout",()=>{
-    itemHelpHome.style.display = "none";
-    clearTimeout(id);
-});
+homeBtn.addEventListener("mouseover",()=>{visible(itemHelpHome)});
+homeBtn.addEventListener("mouseout",()=>{unvisible(itemHelpHome)});
+
+logo.addEventListener("mouseover",()=>{visible(itemHelpLogo)});
+logo.addEventListener("mouseout",()=>{unvisible(itemHelpLogo)});
+
+search.addEventListener("mouseover",()=>{visible(itemHelpSearch)});
+search.addEventListener("mouseout",()=>{unvisible(itemHelpSearch)});
