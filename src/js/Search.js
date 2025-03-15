40f154804a98430a68cee83beaee8ea7a011df70
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-03-16 01:37:10 +0200

    add html search

--- src/js/Search.js
+++ src/js/Search.js
@@ -3,8 +3,33 @@ import {searchAPI} from "./api_search.js";
 const searchInput = document.getElementsByClassName("header-search-input")[0];
 searchInput.addEventListener("change", async ()=>{
     try{
-    console.log(await searchAPI(searchInput.value));
+        const data = await searchAPI(searchInput.value)
+        searchtml(data);
+        console.log(data);
     }catch(error){
         console.error("Ошибка загрузки топ-артистов:", error);
     }
 });
+
+function searchtml(data){
+    document.querySelector(".secti-el").innerHTML = `
+        <div class="search">
+        <div class="artist-track">
+            <div class="artist-serch">
+                <div class="artist-serch-title">Лучший результат</div>
+                <div class="artist-serch-block">
+                    <img class="artist-serch-block-picture" src="" alt="#">
+                    <div class="artist-serch-block-info">
+                        <h2 class="artist-serch-block-name">Name</h2>
+                        <p class="artist-serch-block-text">Исполнитель</p>
+                    </div>
+                </div>
+            </div>
+            <div class="track-serch">
+                <div class="track-serch-title">Треки</div>
+                <div class="track-serch-block"></div>
+            </div>
+        </div>
+    </div>
+    `
+}
