1a639e960c85ac7cff2f6642f28f947122187a4a
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-03-28 23:08:22 +0200

    fix music

--- src/js/Search.js
+++ src/js/Search.js
@@ -128,7 +128,7 @@ function openPageTracks(data, count){
 function spawnItemInPageTracks(data, count){
     for(let i = 0; i < count; i+=1){
         const htmlItem = `
-                    <div class="track-serch-block-item">
+                    <div data-track-id="${i}" class="track-serch-block-item">
                         <div class="track-serch-block-item-info-track">
                             <?xml version="1.0" ?><svg class="button-play" height="18" viewBox="0 0 48 48" width="18" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path d="M-838-2232H562v3600H-838z" fill="none"/><path d="M16 10v28l22-14z"/><path d="M0 0h48v48H0z" fill="none"/></svg>
                             <p class="counter-tracks">${i+1}</p>
@@ -150,19 +150,17 @@ function spawnItemInPageTracks(data, count){
     const buttonPlay = document.querySelectorAll(".track-serch-block-item");
     for(let i = 0; buttonPlay.length > i; i += 1){
         buttonPlay[i].addEventListener("click", (ev)=>{
-            playSearchTrack(data, count)
-            console.log(ev);
-            console.log(data.tracks.items.indexOf(ev.target));
+            playSearchTrack(data, count, ev.target.getAttribute("data-track-id"));
         });
     }
 }
 
-function playSearchTrack(data, count){
+function playSearchTrack(data, count, id){
     const arr = [];
 
     for(let i = 0; i < count; i+=1){
         arr.push(data.tracks.items[i].uri);
     }
 
-    playTrack(arr, 0);
+    playTrack(arr, id);
 }
